e9b5d7ec01c9e5e47176eb9430ca7651952d1a2e
Cyrus Hiatt <chiatt@fargeo.com>
2024-08-04 06:00:07 -0700

    Show list of tracks on home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:trackme/isar_service.dart';
 import '../components/main_menu_drawer.dart';
 
-/// Add a stupid comment
-/// Add anotheer stupid comment
+final isarService = IsarService();
+
+Future<List<String>> getTracesFromDb() async {
+  return [for (var l in await isarService.getLocations()) l.name.toString()];
+}
+
 class HomeScreen extends StatelessWidget {
   /// Constructs a [HomeScreen]
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () => context.go('/auth'),
-            child: const Text('Go to the auth page'),
-          ),
-        ],
-      ),
-    ));
+      appBar: AppBar(title: const Text('Tracks')),
+      body: FutureBuilder(
+        future: getTracesFromDb(),
+        builder: (context, snapshot) {
+        if(snapshot.hasData){
+          final locations = snapshot.data;
+          return Stack(
+            children: [
+              ListView.builder(
+              itemCount: locations!.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 50,
+                  margin: const EdgeInsets.all(2),
+                  color: const Color.fromARGB(255, 219, 255, 252),
+                  child: Center(
+                    child: Text(locations[index],
+                      style: const TextStyle(fontSize: 18),
+                    )
+                  ),
+                );
+              }
+              )
+            ],
+          );
+        } else {
+          return const Stack(
+            children: [],
+          );
+        }
+      }),
+    drawer: const MainMenu());
   }
 }
